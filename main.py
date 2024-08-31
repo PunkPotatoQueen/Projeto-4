e95cb250ef3a1a5972d3f209f4821ff6c813ab1e
MathsGb <108474535+MathsGb@users.noreply.github.com>
2024-08-31 11:29:03 -0300

    Code/Site integracão 0.1

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    
+    if request.method == "POST":
+        selected_eq = request.form.get('eq_select').strip()
+        quantity_eq = request.form.get('eq_quantity').strip()
+
+
+
+@app.route("/calculus",  methods=["GET","POST"])
+def equacoes(selected_eq, quantity_eq):
+    if request.method == "GET":
+        return
+    
+    return render_template('equacoes.html')
 
 
 if __name__ == '__main__':
