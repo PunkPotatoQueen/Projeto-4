5b03fea2fed6113472e1aaf577379a6507dbbb92
MathsGb <108474535+MathsGb@users.noreply.github.com>
2024-09-02 15:45:32 -0300

    Code/Site Integração 0.3v

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for
 from libs.docx_manipulator import DocxGenerator, generate_random_docx
+from utils import *
 
 app = Flask(__name__)
-
 
 @app.route("/", methods=["GET","POST"])
 def index():
@@ -12,32 +12,31 @@ def index():
     if request.method == "POST":
         selected_eq = request.form.get('eq_select').strip()
         quantity_eq = request.form.get('eq_quantity').strip()
-        return equacoes(selected_eq, quantity_eq)
-
-Exemplo = """
-<form action="/equacoes">
-    <label>Massa Padrão:</label>
-    <input type="int" required>
-    <label>Fator de equivalência:</label>
-    <input type="int" required>
-    <label>Volume de NaOH:</label>
-    <input type="int" required>
-</form>
-"""
+        # return equacoes(selected_eq, quantity_eq)
+        return redirect(url_for("equacoes", selected_eq=selected_eq, quantity_eq=quantity_eq))
 
 @app.route("/equacoes",  methods=["GET","POST"])
-def equacoes(selected_eq, quantity_eq):
-    if request.method == "POST":
-
-        result = Exemplo
-        for _ in range(int(quantity_eq) - 1):
-            result += '</br>' + Exemplo
-        print(result)
-
-        return render_template('equacoes.html', result=result)
+def equacoes():
+    selected_eq = request.args.get("selected_eq")
+    quantity_eq = request.args.get("quantity_eq")
+    base = select_base(selected_eq,quantity_eq)
     
-    else: return 
+    if request.method == "GET":
+        return render_template('equacoes.html', result=base)
+    
+    if request.method == "POST":
+        
+        return render_template('equacoes.html', result=base)
+    
 
+@app.route("/resultados", methods=["POST"])
+def gera_resultados():
+    return
+    # if request.method == "POST":
+    #     for i in 
+    #         mp_result = request.form.get[f"MP_result_{i}"]
+    #         fe_result = request.form.get[f"FE_result_{i}"]
+    #         vn_result = request.form.get[f"VN_result_{i}"]
 
 @app.route("/gerar-docx")
 def relatorio_docx():
---
+++ utils.py
@@ -0,0 +1,35 @@
+def select_base(selected_eq , quantity_eq):
+    result=''
+    if selected_eq == "valor 3":
+        for i in range(int(quantity_eq)):
+            base_cm=f"""
+            <label >Equação {i}:</label>
+            <label for="MS_result_{i}">Massa do Soluto:</label>
+            <input type="int" name="MS_result_{i}" id="MS_result_{i}" required>
+            <label for="MM_result_{i}">Massa Molar:</label>
+            <input type="int" name="MM_result_{i}" id="MM_result_{i}" required>
+            <label for="VS_result_{i}">Volume do Soluto:</label>
+            <input type="int" name="VS_result_{i}" id="VS_result_{i}" required>"""
+            result += '<br>' + base_cm
+            i+=1
+
+        return result
+    else: 
+        for i in range(int(quantity_eq)):
+            base_csc= f"""
+            <label >Equação {i}:</label>
+            <label for="MP_result_{i}">Massa Padrão:</label>
+            <input type="int" name="MP_result_{i}" id="MP_result_{i}" required>
+            <label for="FE_result_{i}">Fator de equivalência:</label>
+            <input type="int" name="FE_result_{i}" id="FE_result_{i}" required>
+            <label for="VN_result_{i}">Volume de NaOH:</label>
+            <input type="int" name="VN_result_{i}" id="VN_result_{i}" required>"""
+            if selected_eq == "valor 2":
+                correcao = """
+            <label for= "FC_result_{i}">Fator de Correção:</label>
+            <input type="int" name="FC_result_{i}" id="FC_result_{i}" required>"""
+                base_csc += correcao
+
+            result += '<br>' + base_csc
+            i+=1
+        return result
